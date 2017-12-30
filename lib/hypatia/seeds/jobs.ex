ef24156330a0ed077f4130389b180ae83303b8fa
Damon Aw <daemonsy@gmail.com>
2017-12-30 16:54:35 -0500

    Update seeds to include applications and candidates

--- lib/hypatia/seeds/jobs.ex
+++ lib/hypatia/seeds/jobs.ex
@@ -1,12 +1,12 @@
 defmodule Hypatia.Seeds.Jobs do
   def create_beer_taster(total) do
-    create_beer_taster(total, 0)
+    create_beer_taster(total, 0, [])
   end
 
-  defp create_beer_taster(total, counter) when total == counter, do: true
+  defp create_beer_taster(total, counter, accum) when total == counter, do: accum
 
-  defp create_beer_taster(total, counter) do
-    Hypatia.Repo.insert!(%Hypatia.Job{ title: "#{Faker.Beer.name} Taster" })
-    create_beer_taster(total, counter + 1)
+  defp create_beer_taster(total, counter, accum) do
+    create_beer_taster(total, counter + 1, [
+      Hypatia.Repo.insert!(%Hypatia.Job{ title: "#{Faker.Beer.name} Taster" }) | accum])
   end
 end
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,1 +1,12 @@
+
 Hypatia.Seeds.Jobs.create_beer_taster(10)
+  |> Enum.each(fn(job) ->
+    Hypatia.Repo.insert!(
+      %Hypatia.Job.Application{
+        candidate: %Hypatia.Candidate{
+          first_name: Faker.Name.first_name,
+          last_name: Faker.Name.last_name,
+          email: Faker.Internet.email
+        }
+      }
+  ) end)
